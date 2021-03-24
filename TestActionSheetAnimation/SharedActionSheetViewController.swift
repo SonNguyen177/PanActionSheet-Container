0fc335b2c34b6cbc893e9fa34756bdb57a42ff55
Son Nguyen <nguyenhoaisonfat@gmail.com>
2021-03-24 17:47:35 +0700

    fix some isssue

---
+++ TestActionSheetAnimation/SharedActionSheetViewController.swift
@@ -0,0 +1,322 @@
+//
+//  SharedActionSheetViewController.swift
+//  TestActionSheetAnimation
+//
+//  Created by SonNH-HAV on 3/24/21.
+//
+
+import UIKit
+
+private enum State {
+    case closed
+    case open
+}
+
+extension State {
+    var opposite: State {
+        switch self {
+        case .open: return .closed
+        case .closed: return .open
+        }
+    }
+}
+
+class SharedActionSheetViewController: UIViewController {
+    
+    //MARK: - Constants
+    private var popupOffset: CGFloat = 200
+    private let topOffset: CGFloat = 60
+    private let durationAnimate : TimeInterval = 0.7
+    private let shortDurationAnimate : TimeInterval = 0.3
+    
+    //MARK: - Fields
+    private var currentState: State = .closed
+    
+    // MARK: - Views
+    private lazy var popupView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.1
+        view.layer.shadowRadius = 10
+        return view
+    }()
+    
+    private lazy var swipeImageView: UIImageView = {
+        let imv = UIImageView(image: UIImage(named: "ic_menu_up"))
+        imv.contentMode = .scaleAspectFit
+        return imv
+    }()
+    
+    // customView
+    fileprivate var contentView: UIView!
+    private lazy var defaultContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .orange
+        return view
+    }()
+    
+    // MARK: - View Controller Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+//        view.isOpaque = false
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.hide)))
+        
+        layout()
+       // popupView.addGestureRecognizer(tapRecognizer)
+        popupView.addGestureRecognizer(panRecognizer)
+    }
+    
+    // for add view by demand (AddChildController outer)
+    func setCustomView(_ custom: UIView, preferHeight: CGFloat) {
+        contentView = custom
+        self.popupOffset = preferHeight
+    }
+    
+    private var bottomConstraint = NSLayoutConstraint()
+    
+    private func layout() {
+        popupView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(popupView)
+        popupView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        popupView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomConstraint = popupView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: popupOffset)
+        bottomConstraint.isActive = true
+        popupView.heightAnchor.constraint(equalToConstant: popupOffset + topOffset).isActive = true
+        
+        popupView.addSubview(swipeImageView)
+        swipeImageView.translatesAutoresizingMaskIntoConstraints = false
+        swipeImageView.centerXAnchor.constraint(equalTo: popupView.centerXAnchor).isActive = true
+        swipeImageView.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 0).isActive = true
+        swipeImageView.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        swipeImageView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        if contentView == nil {
+            contentView = defaultContentView
+        }
+        
+        popupView.addSubview(contentView)
+        contentView.isUserInteractionEnabled = true
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: 8).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -8).isActive = true
+        contentView.topAnchor.constraint(equalTo: swipeImageView.bottomAnchor, constant: 0).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: popupView.bottomAnchor).isActive = true
+       
+    }
+    
+    @objc func show(){
+        showInCurrent()
+    }
+    
+    @objc func hide(){
+        currentState = .closed
+        self.animateTransitionIfNeeded(to: currentState, duration: shortDurationAnimate)
+    }
+    
+    fileprivate func showInCurrent(){
+        self.modalPresentationStyle = .overFullScreen
+        topViewController()?.present(self, animated: true, completion: nil)
+        currentState = .open
+        self.animateTransitionIfNeeded(to: currentState, duration: durationAnimate)
+    }
+    
+    fileprivate func topViewController () -> UIViewController? {
+        if let topController = UIApplication.shared.keyWindow?.rootViewController {
+            if let presented = topController.presentedViewController {
+                return presented
+            } else {
+                return topController
+            }
+        }
+        return nil
+    }
+
+    
+    /*
+    //MARK: - Tap Gesture
+   
+    private lazy var tapRecognizer: UITapGestureRecognizer = {
+        let recognizer = UITapGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(popupViewTapped(recognizer:)))
+        return recognizer
+    }()
+    
+    private lazy var panRecognizer: UIPanGestureRecognizer = {
+        let recognizer = UIPanGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
+        return recognizer
+    }()
+    
+    @objc private func popupViewTapped(recognizer: UITapGestureRecognizer) {
+        let state = currentState.opposite
+        let transitionAnimator = UIViewPropertyAnimator(duration: 1, dampingRatio: 1, animations: {
+            switch state {
+            case .open:
+                self.bottomConstraint.constant = 0
+            case .closed:
+                self.bottomConstraint.constant = 440
+            }
+            self.view.layoutIfNeeded()
+        })
+        transitionAnimator.addCompletion { position in
+            switch position {
+            case .start:
+                self.currentState = state.opposite
+            case .end:
+                self.currentState = state
+            case .current:
+                ()
+            }
+            
+            switch self.currentState {
+            case .open:
+                self.bottomConstraint.constant = 0
+            case .closed:
+                self.bottomConstraint.constant = 440
+            }
+        }
+        transitionAnimator.startAnimation()
+    }
+ */
+    
+    //MARK: - Pan Gesture
+    
+    private lazy var panRecognizer: InstantPanGestureRecognizer = {
+        let recognizer = InstantPanGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
+        return recognizer
+    }()
+   
+    /// All of the currently running animators.
+    private var runningAnimators = [UIViewPropertyAnimator]()
+    private var animationProgress: CGFloat = 0
+    @objc private func popupViewPanned(recognizer: UIPanGestureRecognizer) {
+    
+        switch recognizer.state {
+        case .began:
+            
+            // start the animations
+            animateTransitionIfNeeded(to: currentState.opposite, duration: durationAnimate)
+            
+            // pause all animations, since the next event may be a pan changed
+            runningAnimators.forEach{$0.pauseAnimation()}
+            
+            animationProgress = runningAnimators.first?.fractionComplete ?? 0
+            //print("animationProgress = \(animationProgress)")
+        case .changed:
+            
+            // variable setup
+            let translation = recognizer.translation(in: popupView)
+            var fraction = -translation.y / (popupOffset + topOffset)
+            
+            // adjust the fraction for the current state and reversed state
+            if currentState == .open { fraction *= -1 }
+            if runningAnimators[0].isReversed { fraction *= -1 }
+            
+            // apply the new fraction
+            let newComplete =  fraction + animationProgress
+            //print("newComplete = \(newComplete)")
+            runningAnimators.forEach{$0.fractionComplete = newComplete }
+            
+        case .ended:
+            
+            // variable setup
+            let yVelocity = recognizer.velocity(in: popupView).y
+            let shouldClose = yVelocity > 0
+            
+            // if there is no motion, continue all animations and exit early
+            if yVelocity == 0 {
+                runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
+                break
+            }
+            
+            // reverse the animations based on their current state and pan motion
+            switch currentState {
+            case .open:
+                if !shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+                if shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+            case .closed:
+                if shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+                if !shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+            }
+            
+            // continue all animations
+            runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
+            
+        default:
+            ()
+        }
+    }
+    
+    private func animateTransitionIfNeeded(to state: State, duration: TimeInterval) {
+      
+        // ensure that the animators array is empty (which implies new animations need to be created)
+        guard runningAnimators.isEmpty else { return }
+        
+        // an animator for the transition
+        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
+            switch state {
+            case .open:
+                self.bottomConstraint.constant = 0
+                self.popupView.layer.cornerRadius = 20
+            case .closed:
+                self.popupView.layer.cornerRadius = 0
+                self.bottomConstraint.constant = self.popupOffset + self.topOffset // hide all
+            }
+            //
+            self.view.layoutIfNeeded()
+        })
+        
+        // the transition completion block
+        transitionAnimator.addCompletion { position in
+            // update the state
+            switch position {
+            case .start:
+                self.currentState = state.opposite
+            case .end:
+                self.currentState = state
+            case .current:
+                ()
+            }
+            
+            // manually reset the constraint positions
+            var needDissmiss = false
+            switch self.currentState {
+            case .open:
+                self.bottomConstraint.constant = 0
+            case .closed:
+                self.bottomConstraint.constant = self.popupOffset + self.topOffset // hide all
+                needDissmiss = true
+            }
+            
+            // remove all running animators
+            self.runningAnimators.removeAll()
+            
+            if needDissmiss {
+                self.dismiss(animated: false, completion: nil)
+            }
+        }
+        
+        // start all animators
+        transitionAnimator.startAnimation()
+        
+        // keep track of all running animators
+        runningAnimators.append(transitionAnimator)
+    }
+}
+
+// MARK: - InstantPanGestureRecognizer
+/// A pan gesture that enters into the `began` state on touch down instead of waiting for a touches moved event.
+class InstantPanGestureRecognizer: UIPanGestureRecognizer {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        if (self.state == UIGestureRecognizer.State.began) { return }
+        super.touchesBegan(touches, with: event)
+        self.state = UIGestureRecognizer.State.began
+    }
+}
+
+
--- TestActionSheetAnimation/ViewController.swift
+++ TestActionSheetAnimation/ViewController.swift
@@ -10,265 +10,33 @@
 
 import UIKit
 
-private enum State {
-    case closed
-    case open
-}
-
-extension State {
-    var opposite: State {
-        switch self {
-        case .open: return .closed
-        case .closed: return .open
-        }
-    }
-}
-
 class ViewController: UIViewController {
     
-    // MARK: - Constants
-    private let popupOffset: CGFloat = 440
+    let btnShowPopup = UIButton(type: .custom)
     
     // MARK: - View Controller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
-       // popupView.addGestureRecognizer(tapRecognizer)
-        popupView.addGestureRecognizer(panRecognizer)
     }
-    
-    // MARK: - Views
-    private lazy var popupView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .lightGray
-        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        view.layer.shadowColor = UIColor.black.cgColor
-        view.layer.shadowOpacity = 0.1
-        view.layer.shadowRadius = 10
-        return view
-    }()
-    
-    private lazy var swipeImageView: UIImageView = {
-        let imv = UIImageView(image: UIImage(named: "ic_menu_up"))
-        imv.contentMode = .scaleAspectFit
-        return imv
-    }()
-    
-    var contentView: UIView!
-    private lazy var defaultContentView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        return view
-    }()
-    
-    private var bottomConstraint = NSLayoutConstraint()
     
     private func layout() {
-        popupView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(popupView)
-        popupView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        popupView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        bottomConstraint = popupView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 440)
-        bottomConstraint.isActive = true
-        popupView.heightAnchor.constraint(equalToConstant: popupOffset + 60).isActive = true
-        
-        popupView.addSubview(swipeImageView)
-        swipeImageView.translatesAutoresizingMaskIntoConstraints = false
-        swipeImageView.centerXAnchor.constraint(equalTo: popupView.centerXAnchor).isActive = true
-        swipeImageView.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 0).isActive = true
-        swipeImageView.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        swipeImageView.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        
-        if contentView == nil {
-            contentView = defaultContentView
-        }
-        
-        popupView.addSubview(contentView)
-        contentView.isUserInteractionEnabled = false
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: 8).isActive = true
-        contentView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -8).isActive = true
-        contentView.topAnchor.constraint(equalTo: swipeImageView.bottomAnchor, constant: 0).isActive = true
-        contentView.bottomAnchor.constraint(equalTo: popupView.bottomAnchor).isActive = true
+       
+        view.addSubview(btnShowPopup)
+        btnShowPopup.setTitle("Show", for: .normal)
+        btnShowPopup.layer.cornerRadius = 8
+        btnShowPopup.titleLabel?.textColor = UIColor.white
+        btnShowPopup.backgroundColor = .blue
+        btnShowPopup.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+
+        btnShowPopup.translatesAutoresizingMaskIntoConstraints = false
+        btnShowPopup.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        btnShowPopup.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        btnShowPopup.heightAnchor.constraint(equalToConstant: 36).isActive = true
+        btnShowPopup.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 80).isActive = true
     }
     
-    private var currentState: State = .closed
-    
-    /*
-    //MARK: - Tap Gesture
-   
-    private lazy var tapRecognizer: UITapGestureRecognizer = {
-        let recognizer = UITapGestureRecognizer()
-        recognizer.addTarget(self, action: #selector(popupViewTapped(recognizer:)))
-        return recognizer
-    }()
-    
-    private lazy var panRecognizer: UIPanGestureRecognizer = {
-        let recognizer = UIPanGestureRecognizer()
-        recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
-        return recognizer
-    }()
-    
-    @objc private func popupViewTapped(recognizer: UITapGestureRecognizer) {
-        let state = currentState.opposite
-        let transitionAnimator = UIViewPropertyAnimator(duration: 1, dampingRatio: 1, animations: {
-            switch state {
-            case .open:
-                self.bottomConstraint.constant = 0
-            case .closed:
-                self.bottomConstraint.constant = 440
-            }
-            self.view.layoutIfNeeded()
-        })
-        transitionAnimator.addCompletion { position in
-            switch position {
-            case .start:
-                self.currentState = state.opposite
-            case .end:
-                self.currentState = state
-            case .current:
-                ()
-            }
-            
-            switch self.currentState {
-            case .open:
-                self.bottomConstraint.constant = 0
-            case .closed:
-                self.bottomConstraint.constant = 440
-            }
-        }
-        transitionAnimator.startAnimation()
-    }
- */
-    
-    //MARK: - Pan Gesture
-    
-    private lazy var panRecognizer: InstantPanGestureRecognizer = {
-        let recognizer = InstantPanGestureRecognizer()
-        recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
-        return recognizer
-    }()
-   
-    /// All of the currently running animators.
-    private var runningAnimators = [UIViewPropertyAnimator]()
-    private var animationProgress: CGFloat = 0
-    @objc private func popupViewPanned(recognizer: UIPanGestureRecognizer) {
-    
-        switch recognizer.state {
-        case .began:
-            
-            // start the animations
-            animateTransitionIfNeeded(to: currentState.opposite, duration: 0.5)
-            
-            // pause all animations, since the next event may be a pan changed
-            runningAnimators.forEach{$0.pauseAnimation()}
-            
-            animationProgress = runningAnimators.first?.fractionComplete ?? 0
-            //print("animationProgress = \(animationProgress)")
-        case .changed:
-            
-            // variable setup
-            let translation = recognizer.translation(in: popupView)
-            var fraction = -translation.y / popupOffset
-            
-            // adjust the fraction for the current state and reversed state
-            if currentState == .open { fraction *= -1 }
-            if runningAnimators[0].isReversed { fraction *= -1 }
-            
-            // apply the new fraction
-            let newComplete =  fraction + animationProgress
-            //print("newComplete = \(newComplete)")
-            runningAnimators.forEach{$0.fractionComplete = newComplete }
-            
-        case .ended:
-            
-            // variable setup
-            let yVelocity = recognizer.velocity(in: popupView).y
-            let shouldClose = yVelocity > 0
-            
-            // if there is no motion, continue all animations and exit early
-            if yVelocity == 0 {
-                runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
-                break
-            }
-            
-            // reverse the animations based on their current state and pan motion
-            switch currentState {
-            case .open:
-                if !shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
-                if shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
-            case .closed:
-                if shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
-                if !shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
-            }
-            
-            // continue all animations
-            runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
-            
-        default:
-            ()
-        }
-    }
-    
-    private func animateTransitionIfNeeded(to state: State, duration: TimeInterval) {
-      
-        // ensure that the animators array is empty (which implies new animations need to be created)
-        guard runningAnimators.isEmpty else { return }
-        
-        // an animator for the transition
-        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
-            switch state {
-            case .open:
-                self.bottomConstraint.constant = 0
-                self.popupView.layer.cornerRadius = 20
-            case .closed:
-                self.popupView.layer.cornerRadius = 0
-                self.bottomConstraint.constant = self.popupOffset
-            }
-            //
-            self.view.layoutIfNeeded()
-        })
-        
-        // the transition completion block
-        transitionAnimator.addCompletion { position in
-            // update the state
-            switch position {
-            case .start:
-                self.currentState = state.opposite
-            case .end:
-                self.currentState = state
-            case .current:
-                ()
-            }
-            
-            // manually reset the constraint positions
-            switch self.currentState {
-            case .open:
-                self.bottomConstraint.constant = 0
-            case .closed:
-                self.bottomConstraint.constant = self.popupOffset
-            }
-            
-            // remove all running animators
-            self.runningAnimators.removeAll()
-        }
-        
-        // start all animators
-        transitionAnimator.startAnimation()
-        
-        // keep track of all running animators
-        runningAnimators.append(transitionAnimator)
+    @objc func pressButton(){
+        SharedActionSheetViewController().show()
     }
 }
-
-// MARK: - InstantPanGestureRecognizer
-/// A pan gesture that enters into the `began` state on touch down instead of waiting for a touches moved event.
-class InstantPanGestureRecognizer: UIPanGestureRecognizer {
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
-        if (self.state == UIGestureRecognizer.State.began) { return }
-        super.touchesBegan(touches, with: event)
-        self.state = UIGestureRecognizer.State.began
-    }
-}
-
